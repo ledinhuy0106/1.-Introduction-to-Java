47910540089478f23b5a7591654c194b2f65ec49
uyle1234 <91107969+uyle1234@users.noreply.github.com>
2021-10-07 17:35:46 +0700

    Add files via upload

---
+++ Chuyendoitiente.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Chuyendoitiente{
+    public static void main (String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter USD :");
+        double usd;
+        usd = scanner.nextDouble();
+        double vnd = usd * 23000;
+        System.out.println(usd + "equivalent :" + vnd + "VND");
+    }
+}
